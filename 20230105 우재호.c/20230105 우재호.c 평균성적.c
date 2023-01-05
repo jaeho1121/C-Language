6f987d890e8494306205bdbcd84298e285dfb4cd
jaeho1121 <121844179+jaeho1121@users.noreply.github.com>
2023-01-05 14:07:11 +0900

    Add files via upload

---
+++ 20230105 우재호.c/20230105 우재호.c 평균성적.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+main() {
+
+	int i, num, sco[20], max, min, a = 0, b = 0;
+	double add = 0;
+	printf("학생수 입력 : ");
+	scanf("%d", &num);
+	for (int i = 0; i < 20; i++)
+	{
+		sco[i] = rand() % 10;
+		printf("%d ", sco[i]);
+	}
+	max = sco[0];
+	min = sco[0];
+	for (i = 0; i < num; i++) {
+		if (max < sco[i]) {
+			max = sco[i];
+			a = i;
+		}
+		if (min > sco[i]) {
+			min = sco[i];
+			b = i;
+		}
+		add += sco[i];
+	}
+	printf("\n최저점수(%d번 학생) : %d\n", b + 1, min);
+	printf("최고점수(%d번 학생) : %d\n", a + 1, max);
+	printf("평균 : %f\n", add / num);
+}
